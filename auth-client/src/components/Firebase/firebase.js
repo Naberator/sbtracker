7e2fcd96a482ac7cc354b75b46d109ca4128ad49
Ziza <ziv.cyber@gmail.com>
2019-02-04 00:46:27 +0200

    Started working on auth

---
+++ auth-client/src/components/Firebase/firebase.js
@@ -0,0 +1,9 @@
+import * as CREDS from '../../ds.Store1';
+const config = {
+    apiKey: CREDS.API_KEY,
+    authDomain: CREDS.AUTH_DOMAIN,
+    databaseURL: CREDS.DATABASE_URL,
+    projectId: CREDS.PROJECT_ID,
+    storageBucket: CREDS.STORAGE_BUCKET,
+    messagingSenderId: CREDS.MESSAGING_SENDER_ID
+  };
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var express = require('express')
 var app = express()
 var mainRoute = require('./Router/main')
+// Initialize firebase
+var firebase = require('firebase')
+creds = () => { JSON.parse(fs.readFileSync('ds.Store')) }
+var config = {
+    apiKey: creds.apiKey,
+    authDomain: creds.authDomain,
+    databaseURL: creds.databaseURL,
+    storageBucket: creds.storageBucket
+}
 
+firebase.initializeApp(config)
 
 
 app.use('/', mainRoute)
